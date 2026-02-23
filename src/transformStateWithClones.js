73c6f2efa6436a3bf4d76072ece982024576827a
rustem-async <rustemamfoot@gmail.com>
2026-02-23 17:03:58 +0200

    task done

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,40 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  let currentState = { ...state };
+  const history = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'clear':
+        currentState = {};
+        history.push(currentState);
+        break;
+
+      case 'addProperties':
+        currentState = { ...currentState, ...action.extraData };
+        history.push(currentState);
+        break;
+
+      case 'removeProperties':
+        currentState = {
+          ...Object.fromEntries(
+            Object.entries(currentState).filter(
+              ([key]) => !action.keysToRemove.includes(key),
+            ),
+          ),
+        };
+        history.push(currentState);
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return history;
 }
 
 module.exports = transformStateWithClones;
